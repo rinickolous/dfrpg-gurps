4a1223723d5184a0c94eb1e3d03549bfb12dd7f9
rinickolous <mikolajtomczynski@gmail.com>
2020-11-15 01:55:17 +0000

    added IQ/A type for spells

---
+++ scripts/data_common_GURPS4e.lua
@@ -0,0 +1,119 @@
+-- Abilities drop down list data
+aAbilityTypeData = {
+	"Skill",
+	"Spell",
+	"Power",
+	"Other",
+};
+
+aSkillTypeData = {
+	"",
+	"ST/Easy",
+	"ST/Average",
+	"ST/Hard",
+	"ST/Very Hard",
+	"DX/Easy",
+	"DX/Average",
+	"DX/Hard",
+	"DX/Very Hard",
+	"IQ/Easy",
+	"IQ/Average",
+	"IQ/Hard",
+	"IQ/Very Hard",
+	"HT/Easy",
+	"HT/Average",
+	"HT/Hard",
+	"HT/Very Hard",
+	"WILL/Easy",
+	"WILL/Average",
+	"WILL/Hard",
+	"WILL/Very Hard",
+	"PER/Easy",
+	"PER/Average",
+	"PER/Hard",
+	"PER/Very Hard",
+	"Tech/Average",
+	"Tech/Hard",
+};
+
+aSkillTypeAbbrData = {
+	"",
+	"ST/E",
+	"ST/A",
+	"ST/H",
+	"ST/VH",
+	"DX/E",
+	"DX/A",
+	"DX/H",
+	"DX/VH",
+	"IQ/E",
+	"IQ/A",
+	"IQ/H",
+	"IQ/VH",
+	"HT/E",
+	"HT/A",
+	"HT/H",
+	"HT/VH",
+	"Will/E",
+	"Will/A",
+	"Will/H",
+	"Will/VH",
+	"Per/E",
+	"Per/A",
+	"Per/H",
+	"Per/VH",
+	"Tech/A",
+	"Tech/H",
+};
+
+aSpellClassData = {
+	"Regular",
+	"Area",
+	"Melee",
+	"Missile",
+	"Blocking",
+	"Information",
+	"Enchantment",
+	"Special",
+};
+
+aSpellTypeData = {
+	"IQ/Average",
+	"IQ/Hard",
+	"IQ/Very Hard",
+};
+
+aSpellTypeAbbrData = {
+	"IQ/A",
+	"IQ/H",
+	"IQ/VH",
+};
+
+aTraitTypeData = {
+	"Advantage",
+	"Perk",
+	"Disadvantage",
+	"Quirk",
+};
+
+-- Item drop down list data
+aItemTypeData = {
+	"",
+	"Equipment",
+	"Defense",
+	"Melee Weapon",
+	"Ranged Weapon",
+	"Melee Weapon, Ranged Weapon",
+	"Defense, Melee Weapon",
+	"Defense, Ranged Weapon",
+	"Defense, Melee Weapon, Ranged Weapon",
+};
+
+-- Vehicle drop down list data
+aVehicleTypeData = {
+	"",
+	"Ground Vehicle",
+	"Watercraft",
+	"Aircraft",
+	"Spacecraft",
+};
